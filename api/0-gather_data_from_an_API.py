bff60f119cae1b92cd348a1b56ba04a4b0b1c4c3
Rbrogniez <5013@holbertonschool.com>
2022-12-13 13:16:58 +0100

    add files for API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python3
-"""Gather data using the REST API"""
-import requests
-from sys import argv
+"""
+a Python script that, using this REST API, for a given employee ID, returns
+ information about his/her TODO list progress.
+"""
+from requests import get
+from sys import argv as arg
+
 
 if __name__ == '__main__':
-    complete = 0
-    complete_list = []
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    r = requests.get(url).json()
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
-    todos = requests.get(url).json()
-    for task in todos:
-        if task.get('completed'):
-            complete += 1
-            complete_list.append(task.get('title'))
-    print('Employee {} is done with tasks({}/{}):'
-          .format(r.get('name'), complete, len(todos)))
-    for task in complete_list:
-        print('\t {}'.format(task))
+
+    # create api_todos dictionary
+    api_todos_dict = {'userId': arg}
+
+    # and add it to the url
+    todos_url = get('https://jsonplaceholder.typicode.com/todos',
+                    params=api_todos_dict)
+    todos = todos_url.json()
+
+    # create api_user dictionary
+    api_user_dict = {'id': arg}
+
+    # add it to url
+    user_url = get('https://jsonplaceholder.typicode.com/users',
+                   params=api_user_dict)
+    user = user_url.json()
+
+    # define the variables
+    EMPLOYEE_NAME = None
+    TOTAL_NUMBER_OF_TASKS = 0
+    completed = 0
+    NUMBER_OF_DONE_TASKS_title = []
+
+    # find the value of the employee_name
+    EMPLOYEE_NAME = user[0].get('name')
+
+    # The script must accept an integer as a parameter which is the employee ID
+    for j in todos:
+        if j.get('userId') == int(arg[1]):
+            # to add total number of tasks
+            TOTAL_NUMBER_OF_TASKS += 1
+            if j.get('completed') is True:
+                # to add number of done tasks
+                completed += 1
+                NUMBER_OF_DONE_TASKS_title.append(j.get('title'))
+
+
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          completed,
+                                                          TOTAL_NUMBER_OF_TASKS
+                                                          ))
+
+    for i in NUMBER_OF_DONE_TASKS_title:
+        print("{} {}".format('\t', i))
